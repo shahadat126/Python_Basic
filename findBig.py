845e84e5439a55819fb97a93d934666ceda09754
shahadat126 <mdshahadathossainzen@gmail.com>
2024-12-16 16:39:05 +0600

    Find Big number

---
+++ findBig.py
@@ -0,0 +1,12 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a>b & a>c :
+    print("a is big number")
+    
+elif b>a % b>c :
+    print("b is big number")
+    
+else:
+    print("c is big number")
