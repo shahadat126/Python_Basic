295d9485927ffce70ac5fa92f2c1207c426b79de
shahadat126 <mdshahadathossainzen@gmail.com>
2024-12-16 16:23:08 +0600

    Area

--- variable.py
+++ variable.py
@@ -12,11 +12,3 @@
 
 
 
-base = float(input("Enter base: "))
-height = float(input("Enter height: "))
-radius = float(input("Enter radius: "))
-
-Triangle_area = 0.5 * base * height
-Circle_area = 3.1416 * radius * radius
-print("Area of Triangle :", Triangle_area)
-print("Area of Circle", Circle_area)
