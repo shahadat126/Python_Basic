4b7089de917da0f221bf01b1324c976c70749eb3
shahadat126 <mdshahadathossainzen@gmail.com>
2024-12-16 16:58:18 +0600

    Grade Point

---
+++ postive.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+
+if number>0 :
+    print("Number is postive")
+    
+elif number<0 :
+    print("Number is negative")
+    
+else :
+    print("number is Zero")
