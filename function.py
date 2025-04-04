27343e63647b2a9a8cdcac6d09d09fe32b6f3c48
shahadat126 <mdshahadathossainzen@gmail.com>
2025-04-04 12:45:28 +0600

    Function

---
+++ function.py
@@ -0,0 +1,16 @@
+def generate_multiplication_table(n):
+    print(n,"x 1 =",n*1)
+    print(n,"x 1 =",n*2)
+    print(n,"x 1 =",n*3)
+    print(n,"x 1 =",n*4)
+    print(n,"x 1 =",n*5)
+    print(n,"x 1 =",n*6)
+    print(n,"x 1 =",n*7)
+    print(n,"x 1 =",n*8)
+    print(n,"x 1 =",n*9)
+    print(n,"x 1 =",n*10)
+    
+    
+n=int(input("Enter a number"))
+generate_multiplication_table(n)
+generate_multiplication_table(n+1)
