22bf339a5fe2d7580d1435c2bf2efdd98086dc84
shahadat126 <mdshahadathossainzen@gmail.com>
2025-04-04 23:18:24 +0600

    while loop

--- while_loop.py
+++ while_loop.py
@@ -1,5 +1,18 @@
-i = 1
-while i <= 10:
-  print("Hello world")
-  i=i+1
+# i = 1
+# while i <= 10:
+#   print("Hello world")
+#   i=i+1
+
+def multiplication(n):
+    for i in range(1,11):
+        print("{} x {} = {}".format(n,i,n*i))
+   
+    
+n= input("enter a number")
+n=int(n)
+while n!=0:
+  multiplication(n)
+  print(" ")
+  n=input("enter a number")
+  n=int(n)
   
