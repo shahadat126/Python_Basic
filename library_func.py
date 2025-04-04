bab25d4995ecc75ee672f358fdf65a7ad4624d22
shahadat126 <mdshahadathossainzen@gmail.com>
2025-04-04 22:29:46 +0600

    Library function

---
+++ library_func.py
@@ -0,0 +1,8 @@
+# import random
+
+# from library_func import today
+# random_number = random.randint(1,100)
+# print(random_number)
+import datetime
+Today = datetime.datetime.today()
+print(Today)
