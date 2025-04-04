840b6efa98a26c16e9038f3783d3c96e143fb202
shahadat126 <mdshahadathossainzen@gmail.com>
2025-04-04 23:44:29 +0600

    List

--- list.py
+++ list.py
@@ -1,5 +1,33 @@
-fruits =["apple","orange","banana","mango"]
-print(fruits)
-fruits.append("sour")
-fruits.pop
-"banana" in fruits
+# fruits =["apple","orange","banana","mango"]
+# print(fruits)
+# fruits.append("sour")
+# fruits.pop
+li =[1,2,3,4,5]
+print(li)
+li2=[6,7,8,9,10]
+li.extend(li2)
+print(li)
+li2.extend(li)
+print(li2)
+#extend concat two list
+#list1.extend(list2) after this operation its value assigned in list1
+#list.insert(index,element)
+li3=["a","b","c","d","b"]
+li3.insert(0,"e")
+print(li3)
+#list.remove(element)
+li3.remove("b")
+print(li3)
+#list.index(element)
+print(li3.index("a"))
+#list.pop(index)
+x=li3.pop(0)
+print(x)
+print(li3)
+#list.sort()
+li3.sort()
+print(li3)
+#list.reverse()
+li3.reverse()
+print(li3)
+
