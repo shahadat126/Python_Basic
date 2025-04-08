06c2f454b405a05ce0b1ecd22ad67d72ffd43762
shahadat126 <mdshahadathossainzen@gmail.com>
2025-04-08 21:11:38 +0600

    Max & Sum

---
+++ max,min,sum.py
@@ -0,0 +1,17 @@
+# li =[1,3,5,10,100,34,49,9]
+# max_number = max(li)
+# print(max_number)
+# li =[1,3,5,10,100,34,49,9]
+# max_number= float('-inf')
+# for num in li:
+#     if num > max_number:
+#         max_number=num
+# print(max_number)
+
+# result= sum(li)
+# print(result) 
+numbers=[20,30,40,50,60,45,45]
+results = 0
+for num in numbers:
+    results= results + num
+print(results)
