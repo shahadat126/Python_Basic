9a62c80b8af67eb1f2865295e9652d472560a8d6
shahadat126 <mdshahadathossainzen@gmail.com>
2025-04-10 16:37:39 +0600

    Range

---
+++ range.py
@@ -0,0 +1,18 @@
+li = range(1,11)
+print(type(li))
+for x in li:
+    print(x)
+    
+li2 = list(range(1,11))
+
+print(li2)
+
+even_numbers= list(range(2,101,2))
+print(even_numbers)
+
+numbers= list(range(10))
+print(numbers)
+negative2 = list(range(10,0,-1))
+print(negative2)
+negative = list(range(10,0,-5))
+print(negative)
