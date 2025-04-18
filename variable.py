4b3ee6e3e249e8345d1fce3fb8f024dbdf6163eb
shahadat126 <mdshahadathossainzen@gmail.com>
2025-04-18 17:50:19 +0600

    variables & operators

--- variable.py
+++ variable.py
@@ -4,6 +4,14 @@
 # print("hello world")
 # print('hello world')
 
+a = 21//5
+print(a)# integer division
+print(float(a))#convert into float
+
+b=10/5
+print(b)# float division
+print(int(b))#convert into integer
+
 
 
 
