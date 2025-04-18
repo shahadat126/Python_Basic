8ca790d7fbcda9849106ceed57bf07cd2b28b035
shahadat126 <mdshahadathossainzen@gmail.com>
2025-04-18 17:39:51 +0600

    String formatting

---
+++ string_formatting.py
@@ -0,0 +1,14 @@
+# user_input= input("What is your name?")
+# a = "Good morning  {}. How are you ? {}".format(user_input,"HI")
+# print(user_input)
+# print(a)
+
+age = 25
+f_name = "rahim"
+l_name= "khan"
+
+# txt = "i am {f_name} {l_name}. i am {age} years old.".format(l_name=l_name,f_name=f_name,age=age)
+txt = "I am {} {}.I am {} years old.".format(f_name,l_name,age)# long step
+tx2 = f"i am {f_name} {l_name}.I am {age} years old."#short step
+print(txt)
+print(tx2)
