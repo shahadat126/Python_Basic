1dab1c2716886b248e96d388366c6f5d4b22e96b
shahadat126 <mdshahadathossainzen@gmail.com>
2025-04-18 16:25:59 +0600

    String method

---
+++ string_methods.py
@@ -0,0 +1,14 @@
+a ="rahim"
+print(a.title()) #output Rahim
+print(a)# output rahim
+b= a.title()
+print(b)
+c=a.upper()
+print(c)
+x="RAhIM"
+d=x.swapcase() #output raHim
+print(d)
+txt = "i like bananas"
+x=txt.replace("bananas","apples")
+print(x)
+print(x.count('a'))
