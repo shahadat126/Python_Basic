afc1def8313683b21a4a1eece0d17efe92f93119
shahadat126 <mdshahadathossainzen@gmail.com>
2025-04-18 18:11:35 +0600

    Math function

---
+++ math_function.py
@@ -0,0 +1,15 @@
+import math
+a = 4.5
+y = 4.1
+print(math.ceil(a))
+print(math.ceil(y))
+print(math.floor(a))
+print(math.floor(y))
+
+b= 0.004
+print(math.ceil(b))
+print(math.floor(b))
+
+#Round 
+tax = 420.49 # .1 to .5 floor and .51 to .99 ceil
+print(round(tax))
