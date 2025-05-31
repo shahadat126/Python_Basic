40aa850de02c6cb0084ef1b7630ead0f342aca06
shahadat126 <mdshahadathossainzen@gmail.com>
2025-05-31 10:26:17 +0600

    Self, __str__  method

--- Python(OOP)/class2.py
+++ Python(OOP)/class2.py
@@ -3,21 +3,21 @@
 class Students :
    
     def __init__(self,roll,gpa):
-        self.gender= "male"
-        self.city = "dhaka"
+        self.gender= "male".capitalize()
+        self.city = "dhaka".capitalize()
         self.Roll= roll
         self.Gpa= gpa
-        print("hi",self)
+        print(self) #it print  instance from __str__ method what i choose
     def __str__(self):
-        return "Roll : {} \n Gpa : {}".format(self.Roll,self.Gpa)
+        return "Roll : {} | Gpa : {} | Gender: {} | City: {}".format(self.Roll,self.Gpa,self.gender,self.city) #return this instance into print(self) so that it print this return data without object calling
 
 rahim= Students(101,4.5)
-print(rahim.Roll)
+# print(rahim) it will neew when i did not use print(self) into the class
 
-class Car:
-    def __init__(self, model):
-        self.model1 = model  # self refers to car1
+# class Car:
+#     def __init__(self, model):
+#         self.model1 = model  # self refers to car1
 
-car1 = Car("Toyota")
-print(car1.model1)  # Output: Toyota
+# car1 = Car("Toyota")
+# print(car1.model1)  # Output: Toyota
 
--- Python(OOP)/class_method.py
+++ Python(OOP)/class_method.py
@@ -1,6 +1,33 @@
-class Car:
-    @classmethod
-    def drive(cls):
-        print("He is driving a Car")
+# class Car:
+#     @classmethod
+#     def drive(cls):
+#         print("He is driving a Car")
         
-Car.drive()
+# Car.drive()
+class Student:
+    total_students = 0
+    def __init__(self,name):
+        self.name= name
+        Student.total_students +=1
+        
+    @classmethod
+    def get_total_students(cls):
+        return cls.total_students
+    
+s1= Student("shimul")
+s1= Student("shimul1")
+s1= Student("shimul2")
+print(Student.total_students)
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    @classmethod
+    def from_dict(cls, data):
+        name, price = data.split(',')
+        return cls(name,price)
+
+p = Product.from_dict("keya,20")
+print(p.name)   # Output: Shampoo
+print(p.price)  # Output: 120
